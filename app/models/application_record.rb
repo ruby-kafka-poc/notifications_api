7ab993aae01f59903219c1a03238dbe47d225e07
mberrueta <matiasberrueta@gmail.com>
2022-05-13 19:37:35 -0300

    Added mail controller

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
-# # frozen_string_literal: true
-#
-# class ApplicationRecord < ActiveRecord::Base
-#   primary_abstract_class
-# end
+
+class ApplicationRecord < ActiveRecord::Base
+  primary_abstract_class
+end
