8678496be7e4ba4ebf581760aea2daa6de1f3f43
mberrueta <matiasberrueta@gmail.com>
2022-05-31 15:56:08 -0300

    remove env file

--- app/consumers/customer_created_consumer.rb
+++ app/consumers/customer_created_consumer.rb
@@ -2,9 +2,7 @@
 
 class CustomerCreatedConsumer < ApplicationConsumer
   def consume
-    # debugger
   end
 end
 
 # https://github.com/ayyoubjadoo/Karafka-Example/blob/master/app/consumers/callbacked_consumer.rb
-# https://github.com/ayyoubjadoo/Karafka-Example/blob/master/app/consumers/inline_batch_consumer.rb
